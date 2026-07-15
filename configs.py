bf24f0c55a7e22f587fd60414ffe89ea0938f3c4
The Bengineer <bengineeringelm@gmail.com>
2026-07-15 16:22:11 -0400

    Fix Fahrenheit configs using distinct output filenames

--- configs.py
+++ configs.py
@@ -62,20 +62,25 @@ class WLConfig_RGB_Black(WLConfig_RGB_White):
 
 class WLConfig_BW_F(WLConfig_BW):
     TITLE = "BW °F"
+    OUT_FILENAME = "landscape_wb_f"
     TEMPUNITS_MODE = 1
 
 class WLConfig_EINK_F(WLConfig_EINK):
     TITLE = "BW EINK °F"
+    OUT_FILENAME = "landscape_eink_f"
     TEMPUNITS_MODE = 1
 
 class WLConfig_BWI_F(WLConfig_BWI):
     TITLE = "BW inverted °F"
+    OUT_FILENAME = "landscape_wbi_f"
     TEMPUNITS_MODE = 1
 
 class WLConfig_RGB_White_F(WLConfig_RGB_White):
     TITLE = "Color °F, white BG"
+    OUT_FILENAME = "landscape_rgb_w_f"
     TEMPUNITS_MODE = 1
 
 class WLConfig_RGB_Black_F(WLConfig_RGB_Black):
     TITLE = "Color °F, black BG"
+    OUT_FILENAME = "landscape_rgb_b_f"
     TEMPUNITS_MODE = 1
